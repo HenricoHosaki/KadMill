3713c9748cecc9d53fc156de49875510f5485164
HenricoHosaki <henricohosaki@gmail.com>
2026-01-18 11:33:24 -0300

    refactor: add relatorios routes

--- backend/src/routes/administradores.ts
+++ backend/src/routes/administradores.ts
@@ -4,11 +4,11 @@ import { AdministradorController } from '../controllers/administradores';
 import { apenasAdmin, autenticadorMiddleware } from '../middlewares/authMiddeware';
 const administradorController = new AdministradorController();
 
+router.get('/administradores/backup', autenticadorMiddleware, apenasAdmin, (req, res) => administradorController.fazerBackup(req, res));
 router.get('/administradores', autenticadorMiddleware, apenasAdmin,(req, res) => administradorController.pegarTodosUsuarios(req, res));
 router.get('/administradores/:id', autenticadorMiddleware, apenasAdmin, (req, res) => administradorController.pegarUsuarioPorId(req, res));
 router.post('/administradores', autenticadorMiddleware, apenasAdmin, (req, res) => administradorController. criarUsuario(req, res));
 router.put('/administradores/:id', autenticadorMiddleware, apenasAdmin, (req, res) => administradorController.atualizarUsuario(req, res));
 router.delete('/administradores/:id', autenticadorMiddleware, apenasAdmin, (req, res) => administradorController.deletarUsuario(req, res));
-router.get('/administradores/backup', autenticadorMiddleware, apenasAdmin, (req, res) => administradorController.fazerBackup(req, res));
 
 export default router;
