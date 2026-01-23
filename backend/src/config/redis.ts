aa41731e3a3b7f06d08335fe4d8c2fc740bb58cf
HenricoHosaki <henricohosaki@gmail.com>
2026-01-23 10:07:36 -0300

    refactor: add redis password;

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -3,6 +3,8 @@ import Redis from "ioredis";
 export const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
+  ...(process.env.REDIS_PASSWORD ? { password: process.env.REDIS_PASSWORD } : {}),
+  family: 0,
 });
 
 redis.on("connect", () => {
